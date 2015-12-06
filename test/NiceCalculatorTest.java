d67a1a81b0f224209034cd8ec8358e9b94e23cc5
Chris Sawczuk <gummybassist@gmail.com>
2015-12-06 22:18:16 +0000

    initial test failing

---
+++ test/NiceCalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NiceCalculatorTest {
+
+    @Test
+    public void canDetectAWordWithDoubleCharacters()
+    {
+        NiceCalculator niceCalculator = new NiceCalculator();
+
+        assertThat(niceCalculator.containsDouble("aaa"), is(true));
+    }
+
+    private class NiceCalculator {
+
+        public boolean containsDouble(String input) {
+            return false;
+        }
+    }
+}
